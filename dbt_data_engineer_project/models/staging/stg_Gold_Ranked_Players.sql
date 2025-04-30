df18ddaf03422df944ae97303261e92e0f10ded8
Lucaszhuokaiyu <112446521+Lucaszhuokaiyu@users.noreply.github.com>
2025-04-30 22:26:21 +0000

    a

---
+++ dbt_data_engineer_project/models/staging/stg_Gold_Ranked_Players.sql
@@ -0,0 +1,18 @@
+WITH raw_gold AS (
+    SELECT *
+    FROM {{ source('data_engineer_project', 'Gold_Ranked_Players') }}
+),
+stg_gold AS (
+    SELECT
+        "leagueId" AS league_id,
+        wins,
+        losses,
+        veteran,
+        inactive,
+        freshBlood AS fresh_blood,
+        hotStreak AS hot_streak,
+        CURRENT_TIMESTAMP AS loaded_at
+    FROM raw_gold
+)
+SELECT *
+FROM stg_gold
